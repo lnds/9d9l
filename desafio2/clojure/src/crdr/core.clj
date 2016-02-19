48316252cfc796ebc992d7f1f58501d3cb8ac43e
Eduardo  Díaz Cortés <ediaz@dcc8090.cl>
2016-02-19 10:31:55 -0300

    Falta ordenar el resultado.

--- desafio2/clojure/src/crdr/core.clj
+++ desafio2/clojure/src/crdr/core.clj
@@ -1,47 +1,25 @@
 (ns crdr.core (:gen-class))
 
-(require '[clojure.xml :as xml])
 
-(defn format-date [date]
-	(let [fmt (java.time.format.DateTimeFormatter/ofPattern "yyyy-MM-dd HH:mm:ss")]
-		(.format date fmt)))
+(use 'crdr.news)
 
-(defn parse-date [fmt date]
-	(java.time.LocalDateTime/parse date fmt))
+(defn pr-news [n]
+	(println "Título: " (:title n))
+	(println "Fuente: " (:source n))
+	(println "Fecha: " (:pub n))
+	(doseq [line (take 3 (:body n))]
+		(println (subs line 0 (min (count line) 140))))
+	(println))
 
-(def parse-rfc1123  (partial parse-date  java.time.format.DateTimeFormatter/RFC_1123_DATE_TIME))
-
-(defn extract-tag-content [tag content]
-	(let [elem (filter #(= tag (:tag %)) (:content content))]
-		(first (:content (last elem)))))
-
-(defn item-title [item] (extract-tag-content :title item))
-(defn item-body  [item] (extract-tag-content :description item))
-(defn item-pub   [item] 
-	(let [date (parse-rfc1123 (extract-tag-content :pubDate item))]
-		(format-date date)))
-
-(defn item-data [item]
-	{:title (item-title item), :pub (item-pub item), :body (item-body item)})
-
-;(defn item-title [item] 
-;	(let [title (filter #(= :title (:tag %)) (:content item))]
-;		(first (:content (last title)))))
-
-(defn parse-atom [content] (println "atom"))
-
-(defn parse-rss  [content]
-	(let [items (filter #(= :item (:tag %)) (:content (first content)))]
-		(println (map item-data items))))
-
-(defn parse-news [source] 
-	(let [x (xml/parse source)]
-		(condp = (:tag x)
-			:feed (parse-atom (:content x))
-			:rss  (parse-rss  (:content x))
-			(str "unexpected format"))))
+(defn print-sorted-news [news]
+	(println "NEWS" (count news))
+	(let [sorted-news (take 10 (sort-by #(> 0 (compare  (:pub %) (:pub %))) news))]
+		(doseq [n sorted-news]
+			(pr-news n))))
 
 (defn -main
   "read an parse feeds"
-  [url]
-  (parse-news url))
+  [& urls]
+  	(print-sorted-news (flatten (pmap parse-news urls)))
+  	(shutdown-agents))
+  
