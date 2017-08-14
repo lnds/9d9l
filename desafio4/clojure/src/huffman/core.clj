3754d7532a422cbbfe18e4097317016aca9240a9
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2017-08-14 11:39:50 -0300

    core skeleton

--- desafio4/clojure/src/huffman/core.clj
+++ desafio4/clojure/src/huffman/core.clj
@@ -1,7 +1,21 @@
 (ns huffman.core
   (:gen-class))
 
+(defn usage [] (println "Uso: huffman [-c|-d] archivo_entrada archivo_salida"))
+
+(defn compress [input output])
+
+(defn decompress [input output])
+
+(defn process [[cmd input-file output-file]]
+  (case cmd
+    "-c" (compress input-file output-file)
+    "-d" (decompress input-file output-file)
+    (usage)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Huffman encoding decoding"
   [& args]
-  (println "Hello, World!"))
+  (if (or (empty? args) (not= 3 (count args)))
+    (usage)
+    (process args)))
