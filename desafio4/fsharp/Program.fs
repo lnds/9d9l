5c1cf24a394147508c4cafcee80f7df89731121b
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2019-01-22 12:58:09 -0300

    refactor fsharp para usar dotnet

---
+++ desafio4/fsharp/Program.fs
@@ -0,0 +1,18 @@
+﻿namespace Huffman
+
+module Program = 
+
+    open Huffman.Trees
+
+    let usage () = printfn "uso: huffman [-c|-d] entrada salida"
+    
+    [<EntryPoint>]
+    let main argv = 
+        if argv.Length <> 3 then
+           usage()
+        else
+           match List.ofArray argv with
+             | param :: input :: output :: [] when param = "-c" -> compress input output 
+             | param :: input :: output :: [] when param = "-d" -> decompress input output
+             | _ -> usage()
+        0 // return an integer exit code
