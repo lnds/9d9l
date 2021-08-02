084381fe653b7e8b07700e25d0137bff8d02c651
Eduardo Díaz <ediaz@dcc8090.cl>
2021-08-02 18:05:01 -0400

    clojure 1.8 para desafio 2

--- desafio2/clojure/project.clj
+++ desafio2/clojure/project.clj
@@ -1,6 +1,6 @@
 (defproject weather "0.1.0-SNAPSHOT"
   :description "weather - concurrent weather reader"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot weather.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
